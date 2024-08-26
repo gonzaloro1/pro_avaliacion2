d5b57c63d191844be7733fd8579c4f4e8a3a208e
gonzaloro1 <gonzaloro1@gmail.com>
2024-08-26 19:09:10 +0200

    26/08
    202

---
+++ compilador201_202/compi202.java
@@ -0,0 +1,36 @@
+package compilador201_202;
+
+import misClases.avaliacion2.CDameDato;
+
+class compi202 {
+
+	public static void main(String[] args) {
+		CDameDato dato = new CDameDato();
+		int numero;
+
+		do {
+			numero = dato.dameOpcion();
+			switch (numero) {
+			case 1:
+				dato.recolleInt();
+				dato.amosaInt();
+				break;
+			case 2:
+				dato.recolleFloat();
+				dato.amosaFloat();
+				break;
+			case 3:
+				dato.recolleString();
+				dato.amosaString();
+				break;
+			case 4:
+				System.out.println("FINAL");
+				break;
+			default:
+				System.out.println("OPCION Erronea");
+				break;
+			}
+		} while (numero != 4);
+
+	}
+}
